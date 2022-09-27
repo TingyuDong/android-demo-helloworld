55937f164740b3cda3400d4a6c7292021db04980
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-09-27 23:44:22 +0800

    feat:add sign

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -2,10 +2,15 @@ package com.thoughtworks.androidtrain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    object List{
+        const val TAG:String="MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(List.TAG,"onCreate")
     }
 }
