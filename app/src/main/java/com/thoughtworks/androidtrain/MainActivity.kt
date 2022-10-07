c9cec9dc3870a0315fe3b049128dc975b5e92c4b
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-10-08 01:38:36 +0800

    feat:frame-layout

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scroll)
+        setContentView(R.layout.activity_frame_homework)
         Log.i(List.TAG,"onCreate")
     }
 }
