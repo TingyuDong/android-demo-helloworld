67d14f83b7a6c5e31a86f27ebec5a8c6a0d3b507
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-10-19 15:03:18 +0800

    feat:constraint-layout

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.thoughtworks.androidtrain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     object List{
@@ -10,7 +12,24 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_frame_homework)
-        Log.i(List.TAG,"onCreate")
+        setContentView(R.layout.activity_main)
+        Log.i(List.TAG,"onCreate").apply {  }
+    }
+
+    fun sendMessage(view: View){
+//        val editText = findViewById<EditText>(R.id.reminder)
+//        val message = editText.text.toString()
+//        val intent = Intent(this, ConstraintAtivity::class.java).apply {
+//            this.putExtra(EXTRA_MESSAGE, message)
+//            this.putExtra(EXTRA_MESSAGE, message)
+//            this.putExtra(EXTRA_MESSAGE, message)
+//            this.putExtra(EXTRA_MESSAGE, message)
+//        }
+        val intent = Intent(this, ConstraintActivity::class.java)
+//        intent.putExtra(EXTRA_MESSAGE, message)
+//        intent.putExtra(EXTRA_MESSAGE, message)
+//        intent.putExtra(EXTRA_MESSAGE, message)
+//        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivity(intent)
     }
 }
