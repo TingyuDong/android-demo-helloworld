e1b612ccf95bf880b1f0b0a18a19b371eeab2869
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-10-25 02:12:26 +0800

    feat:add Button event

--- app/src/main/java/com/thoughtworks/androidtrain/LanguageSelectionActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/LanguageSelectionActivity.kt
@@ -2,10 +2,24 @@ package com.thoughtworks.androidtrain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LanguageSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language_selection)
+        addBtnEvent()
     }
+
+    private fun addBtnEvent() {
+        val btn_android: Button = findViewById(R.id.btn_android)
+        val btn_java: Button = findViewById(R.id.btn_java)
+        btn_android.setOnClickListener {
+        }
+        btn_java.setOnClickListener {
+        }
+
+    }
+
+
 }
