fdf49ee68a8ed084a69f518500e967c4b56aa4df
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-10-25 23:15:47 +0800

    feat:convert json string to tweet list

--- app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.thoughtworks.androidtrain.model.Tweet
 
 class TweetsActivity : AppCompatActivity() {
     object JsonData {
-        const val data = "{\"content\":\"我毕业了\"," +
+        const val data = "[{\"content\":\"我毕业了\"," +
                 "\"sender\":{\"username\":\"Aiolos\",\"nick\":\"oo\",\"avatar\":\"avatar.png\"}," +
-                "\"images\":null,\"comments\":null,\"error\":\"null\",\"unknownError\":null}"
+                "\"images\":null,\"comments\":null,\"error\":\"null\",\"unknownError\":null}]"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +21,8 @@ class TweetsActivity : AppCompatActivity() {
     }
 
     private fun getJson() {
-        val tweet = Gson().fromJson(JsonData.data, Tweet::class.java)
-        Toast.makeText(this, tweet.toString(), Toast.LENGTH_SHORT).show()
+        val type = object : TypeToken<List<Tweet>>() {}.type
+        val tweets = Gson().fromJson<List<Tweet>>(JsonData.data, type)
+        Toast.makeText(this, tweets[0].toString(), Toast.LENGTH_SHORT).show()
     }
 }
