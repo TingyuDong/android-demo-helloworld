129f3e99edc9334a6cbd333cb66c8b883c364cf7
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-10-28 14:38:49 +0800

    feat:add tweet in json

--- app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
@@ -46,6 +46,16 @@ class TweetsActivity : AppCompatActivity() {
                 "是否他一人所作等一系列问题都有不同看法，形成“荷马问题”。\"," +
                 "\"sender\":{\"username\":\"Aiolos\",\"nick\":\"oo\",\"avatar\":\"avatar.png\"}," +
                 "\"images\":null,\"comments\":null,\"error\":\"null\",\"unknownError\":null}," +
+                "{\"content\":\"古希腊人一直将荷马史诗视作希腊文化的精华将荷马视作民族的骄傲，但丁更称荷马为" +
+                "“诗人之王”。但自17世纪末以来，渐渐开始有学者对于荷马是否确有其人，以及他的籍贯、生活年代、史诗" +
+                "是否他一人所作等一系列问题都有不同看法，形成“荷马问题”。\"," +
+                "\"sender\":{\"username\":\"Aiolos\",\"nick\":\"oo\",\"avatar\":\"avatar.png\"}," +
+                "\"images\":null,\"comments\":null,\"error\":\"null\",\"unknownError\":null}," +
+                "{\"content\":\"古希腊人一直将荷马史诗视作希腊文化的精华将荷马视作民族的骄傲，但丁更称荷马为" +
+                "“诗人之王”。但自17世纪末以来，渐渐开始有学者对于荷马是否确有其人，以及他的籍贯、生活年代、史诗" +
+                "是否他一人所作等一系列问题都有不同看法，形成“荷马问题”。\"," +
+                "\"sender\":{\"username\":\"Aiolos\",\"nick\":\"oo\",\"avatar\":\"avatar.png\"}," +
+                "\"images\":null,\"comments\":null,\"error\":\"null\",\"unknownError\":null}," +
                 "{\"content\":\"我上初中了\"," +
                 "\"sender\":{\"username\":\"Aiolos\",\"nick\":\"oo\",\"avatar\":\"avatar.png\"}," +
                 "\"images\":null,\"comments\":null,\"error\":\"null\",\"unknownError\":null}" +
