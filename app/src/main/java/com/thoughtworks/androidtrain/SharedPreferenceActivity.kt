2617810d5d90c5789a874061aec76e364f683b7a
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-01 11:40:57 +0800

    feat:could change textView Content

--- app/src/main/java/com/thoughtworks/androidtrain/SharedPreferenceActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/SharedPreferenceActivity.kt
@@ -1,11 +1,43 @@
 package com.thoughtworks.androidtrain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.thoughtworks.androidtrain.utils.SharedPreferenceUtils
+
+
+private const val IS_HINT_SHOWN = "isHintShown"
 
 class SharedPreferenceActivity : AppCompatActivity() {
+    private val sharedPreferenceUtils = SharedPreferenceUtils()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preference)
+        initUI()
+        addBtnEvent()
+    }
+
+    private fun addBtnEvent() {
+        val btnHint: Button = findViewById(R.id.btn_hint)
+        btnHint.setOnClickListener {
+            sharedPreferenceUtils.writeBoolean(this, IS_HINT_SHOWN, false)
+            onPause()
+        }
+    }
+
+    private fun initUI() {
+        val textView: TextView = findViewById(R.id.tip)
+        val textShow: String = resources.getString(R.string.tips_shown)
+        val textNotShow: String = resources.getString(R.string.tips_not_shown)
+        val isHintShown: Boolean? = sharedPreferenceUtils.readBoolean(this, IS_HINT_SHOWN, true)
+        if (isHintShown == true) textView.text = textShow
+        else textView.text = textNotShow
+    }
+
+    override fun onPause() {
+        super.onPause()
+        startActivity(Intent(this,MainActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/thoughtworks/androidtrain/utils/SharedPreferenceUtils.kt
@@ -0,0 +1,19 @@
+package com.thoughtworks.androidtrain.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreferenceUtils {
+    fun writeBoolean(activity: Activity, key: String, value: Boolean) {
+        val sharedPref: SharedPreferences = activity.getPreferences(Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.putBoolean(key, value)
+        editor.apply()
+    }
+
+    fun readBoolean(activity: Activity, key: String, defaultValue: Boolean) : Boolean {
+        val sharedPref: SharedPreferences = activity.getPreferences(Context.MODE_PRIVATE)
+        return sharedPref.getBoolean(key, defaultValue)
+    }
+}
