fd320053cfbe9817d36a4975d5d44977eccef8f4
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-03 17:53:38 +0800

    feat：join repository

--- app/src/main/java/com/thoughtworks/androidtrain/TweetApplication.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetApplication.kt
@@ -6,21 +6,8 @@ import com.thoughtworks.androidtrain.data.repository.Repository
 import com.thoughtworks.androidtrain.data.source.local.room.AppDatabase
 
 class TweetApplication : Application() {
-    private lateinit var db: AppDatabase
-
     override fun onCreate() {
         super.onCreate()
         Repository.initialize(this)
-        initDatabase()
-    }
-
-    fun getDb() : AppDatabase {
-        return db
-    }
-
-    private fun initDatabase() {
-        db =
-            Room.databaseBuilder(applicationContext, AppDatabase::class.java, "tweet")
-                .allowMainThreadQueries().build()
     }
 }
