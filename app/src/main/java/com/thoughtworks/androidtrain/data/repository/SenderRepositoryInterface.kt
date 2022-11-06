5a4536b88cbaa625b4a143d5816a78689fddf10c
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-06 11:53:42 +0800

    refactor: move interface into repository file

--- app/src/main/java/com/thoughtworks/androidtrain/data/repository/SenderRepositoryInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.thoughtworks.androidtrain.data.repository
-
-import com.thoughtworks.androidtrain.data.model.Sender
-
-interface SenderRepositoryInterface {
-    fun getSender(senderId: Int): Sender?
-    fun addSender(sender: Sender?): Long?
-}
