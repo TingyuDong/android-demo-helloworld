96d3692e4bb69e09cf72a3c12c3421f7e92f1415
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-08 23:32:15 +0800

    feat: click the avatar to show big avatar

--- app/src/main/java/com/thoughtworks/androidtrain/ComposeActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/ComposeActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -110,15 +113,21 @@ class ComposeActivity : AppCompatActivity() {
     @Composable
     private fun Avatar(avatar: String?) {
         val painter = rememberAsyncImagePainter(avatar)
+        val showDialog = remember {
+            mutableStateOf(false)
+        }
         Image(
             modifier = Modifier
                 .size(40.dp)
-                .clip(CircleShape),
+                .clip(CircleShape)
+                .clickable { showDialog.value = true },
             painter = painter,
             contentDescription = "avatar",
             contentScale = ContentScale.Crop
         )
-        BigAvatar(painter)
+        if (showDialog.value) {
+            BigAvatar(painter)
+        }
     }
 
     @Composable
