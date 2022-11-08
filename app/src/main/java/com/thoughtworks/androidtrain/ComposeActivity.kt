2e54c034e4ceb26e6b79ca655156baba967fd265
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-08 22:53:02 +0800

    feat: Show the avatar from the source network

--- app/src/main/java/com/thoughtworks/androidtrain/ComposeActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/ComposeActivity.kt
@@ -7,19 +7,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.thoughtworks.androidtrain.data.model.Sender
@@ -76,10 +77,12 @@ class ComposeActivity : AppCompatActivity() {
     @Composable
     private fun TweetItem(tweet: Tweet) {
         Row(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+            val painter = rememberAsyncImagePainter(tweet.sender?.avatar)
             Image(
-                modifier = Modifier.clip(RoundedCornerShape(4.dp)),
-                painter = painterResource(id = R.drawable.avatar),
-                contentDescription = "avatar"
+                modifier = Modifier.size(40.dp).clip(CircleShape),
+                painter = painter,
+                contentDescription = "avatar",
+                contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.width(8.dp))
             Column {
