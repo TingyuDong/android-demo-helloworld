0ccb02a0b58cc86f66dfecc78c2ec7670aa94a46
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-16 15:56:38 +0800

    refactor:delete redundant code

--- app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
@@ -37,43 +37,7 @@ class TweetsActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         tweetsAdapter = TweetsAdapter(tweets)
         recyclerView.adapter = tweetsAdapter
-//        createTweets()
-//        tweetsAdapter.setTweet(tweets)
     }
-
-//    private fun createTweets(){
-//        val adapterType = object : TypeToken<ArrayList<Tweet?>>() {}.type
-//        val repositoryType = object : TypeToken<ArrayList<Tweet>>() {}.type
-//        val jsonString = JSONResourceUtils().jsonResourceReader(resources, R.raw.tweets)
-//        tweets.addAll(Gson().fromJson<ArrayList<Tweet?>?>(jsonString, adapterType))
-//        tweetRepository.addAllTweet(Gson().fromJson(jsonString, repositoryType))
-//        tweets.clear()
-
-//        tweets.addAll(tweetRepository.fetchTweets())
-//        getTweetFromNet()
-//        addEmptyData()
-//    }
-
-//    private fun getTweetFromNet() {
-//        val tweetsActivity = this
-//        launch(Dispatchers.Main) {
-//            withContext(Dispatchers.IO) {
-//                val url = "https://thoughtworks-mobile-2018.herokuapp.com/user/jsmith/tweets"
-//                val request = Request.Builder()
-//                    .url(url)
-//                    .build()
-//                val response =
-//                    (application as TweetApplication).getHttpClient().newCall(request).execute()
-//                val obj = Objects.requireNonNull(response.body?.string())
-////                var result = response.body.toString()
-//                val type = object : TypeToken<ArrayList<Tweet>>() {}.type
-//                val tweetsFromNetwork = Gson().fromJson<ArrayList<Tweet>?>(obj, type)
-//                tweetRepository.addAllTweet(tweetsFromNetwork)
-//            }
-//            Toast.makeText(tweetsActivity, "加载完毕", Toast.LENGTH_SHORT).show()
-//        }
-//        Toast.makeText(tweetsActivity, "加载中", Toast.LENGTH_SHORT).show()
-//    }
 
     private fun addEmptyData() {
         tweets.add(null)
