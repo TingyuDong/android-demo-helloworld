340e97326aeeb900a6611253cb406ab245d2d240
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-17 16:48:57 +0800

    fix:delete redundant code

--- app/src/main/java/com/thoughtworks/androidtrain/compose/ComposeActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/compose/ComposeActivity.kt
@@ -3,21 +3,13 @@ package com.thoughtworks.androidtrain.compose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import com.thoughtworks.androidtrain.R
 import com.thoughtworks.androidtrain.TweetApplication
 import com.thoughtworks.androidtrain.TweetsViewModel
-import com.thoughtworks.androidtrain.data.model.Sender
-import com.thoughtworks.androidtrain.data.model.Tweet
 import com.thoughtworks.androidtrain.data.repository.*
 import com.thoughtworks.androidtrain.data.source.remote.TweetsRemoteDataSource
 import com.thoughtworks.androidtrain.usecase.AddCommentUseCase
 import com.thoughtworks.androidtrain.usecase.AddTweetUseCase
 import com.thoughtworks.androidtrain.usecase.FetchTweetsUseCase
-import com.thoughtworks.androidtrain.utils.JSONResourceUtils
 import kotlinx.coroutines.Dispatchers
 
 //// Inject Library
@@ -29,47 +21,16 @@ import kotlinx.coroutines.Dispatchers
 //}
 
 class ComposeActivity : AppCompatActivity() {
-    private var tweets = ArrayList<Tweet>()
     private lateinit var tweetsViewModel: TweetsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViewModel()
-        tweets.addAll(
-            Gson().fromJson(
-                JSONResourceUtils().jsonResourceReader(resources, R.raw.tweets),
-                object : TypeToken<ArrayList<Tweet>>() {}.type
-            )
-        )
         setContent {
             TweetScreen(
                 tweetsViewModel = tweetsViewModel
             )
         }
-    }
-
-    @Preview
-    @Composable
-    fun ContentPreview() {
-        tweets.add(getTweet())
-        tweets.add(getTweet())
-    }
-
-    private fun getTweet(): Tweet {
-        val sender = Sender(
-            username = "Aiolia",
-            nick = "Aio",
-            avatar = "https://thoughtworks-mobile-2018.herokuapp.com/images/user/avatar/001.jpeg"
-        )
-        return Tweet(
-            id = 7,
-            content = "沙发",
-            sender = sender,
-            images = null,
-            comments = null,
-            error = null,
-            unknownError = null
-        )
     }
 
     private fun initViewModel() {
