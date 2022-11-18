807121ffd7cf803f4e41fd85c26e465f75930046
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-18 10:02:48 +0800

    feat:login and store userInfo

--- app/src/main/java/com/thoughtworks/androidtrain/LoginActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/LoginActivity.kt
@@ -1,16 +1,36 @@
 package com.thoughtworks.androidtrain
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+    private val editor: SharedPreferences.Editor =
+        getSharedPreferences("UserInfo", 0).edit()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
+        addBtnEvent()
         // Hide the status bar.
         // Remember that you should never show the action bar if the
         // status bar is hidden, so hide that too if necessary.
         actionBar?.hide()
         supportActionBar?.hide()
+    }
+
+    private fun addBtnEvent() {
+        val btnLogin: Button = findViewById(R.id.login_button)
+        btnLogin.setOnClickListener {
+            setUserInfo()
+        }
+    }
+
+    private fun setUserInfo() {
+        editor.putString("UserName", "you")
+        editor.putString("Nick", "you")
+        editor.putString("Avatar", "avatar.png")
+        editor.commit()
     }
 }
