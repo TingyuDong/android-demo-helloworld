83c1bdb7fb5e81f486261e52562eb2143c8a2724
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-22 02:57:40 +0800

    fix:use koin

--- app/src/main/java/com/thoughtworks/androidtrain/TweetApplication.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetApplication.kt
@@ -1,8 +1,10 @@
 package com.thoughtworks.androidtrain
 
 import android.app.Application
-import com.thoughtworks.androidtrain.data.repository.DatabaseRepository
+import com.thoughtworks.androidtrain.data.repository.*
+import com.thoughtworks.androidtrain.modules.appModules
 import okhttp3.OkHttpClient
+import org.koin.core.context.startKoin
 
 class TweetApplication : Application() {
     private lateinit var client: OkHttpClient
@@ -12,6 +14,9 @@ class TweetApplication : Application() {
         DatabaseRepository.initialize(this)
         DatabaseRepository.get().getDatabase()
         client = OkHttpClient()
+        startKoin {
+            modules(appModules)
+        }
     }
 
     fun getHttpClient(): OkHttpClient {
