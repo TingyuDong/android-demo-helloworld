b80f6812b95e5beb17d6bd16b5867e6d7555009a
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-21 13:21:46 +0800

    fix:replace ActivityTestRule by ActivityScenarioRule

--- app/src/androidTest/java/com/thoughtworks/androidtrain/MainActivityTest.kt
+++ app/src/androidTest/java/com/thoughtworks/androidtrain/MainActivityTest.kt
@@ -4,9 +4,9 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith
 @LargeTest
 class MainActivityTest {
     @get:Rule
-    val activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+    val activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun should_be_checked_if_click_the_check_box() {
