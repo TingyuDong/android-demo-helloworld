07f03fb287e052f5ff10f688d710b1d2ff2116ae
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-22 16:52:56 +0800

    fix:remove unnecessary DatabaseRepository.kt

--- app/src/main/java/com/thoughtworks/androidtrain/TweetApplication.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetApplication.kt
@@ -1,7 +1,6 @@
 package com.thoughtworks.androidtrain
 
 import android.app.Application
-import com.thoughtworks.androidtrain.data.repository.*
 import com.thoughtworks.androidtrain.modules.appModules
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
@@ -12,16 +11,10 @@ class TweetApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        DatabaseRepository.initialize(this)
-        DatabaseRepository.get().getDatabase()
         client = OkHttpClient()
         startKoin {
             androidContext(this@TweetApplication)
             modules(appModules)
         }
-    }
-
-    fun getHttpClient(): OkHttpClient {
-        return client
     }
 }
