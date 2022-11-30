792865c5871afb0d0a4e9fd5a11c61b6376dc2c6
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-11-30 13:12:24 +0800

    feat:add test

--- app/src/test/java/com/thoughtworks/androidtrain/TweetsVIewModelTest.kt
+++ app/src/test/java/com/thoughtworks/androidtrain/TweetsVIewModelTest.kt
@@ -1,10 +1,15 @@
 package com.thoughtworks.androidtrain
 
 import android.app.Application
+import android.support.test.runner.AndroidJUnit4
+import androidx.test.core.app.ApplicationProvider
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.thoughtworks.androidtrain.data.model.Tweet
 import com.thoughtworks.androidtrain.usecase.AddCommentUseCase
 import com.thoughtworks.androidtrain.usecase.AddTweetUseCase
 import com.thoughtworks.androidtrain.usecase.FetchTweetsUseCase
+import com.thoughtworks.androidtrain.utils.JSONResourceUtils
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -21,21 +26,28 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
-import org.mockito.junit.MockitoJUnitRunner
+import org.robolectric.annotation.Config
+import kotlin.collections.ArrayList
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(MockitoJUnitRunner::class)
+//@RunWith(RobolectricTestRunner::class)
+//@RunWith(MockitoJUnitRunner::class)
+@Config(sdk = [29])
+@RunWith(AndroidJUnit4::class)
 class TweetsVIewModelTest {
     @Mock
     private lateinit var fetchTweetsUseCase: FetchTweetsUseCase
+
     @Mock
     private lateinit var addCommentUseCase: AddCommentUseCase
+
     @Mock
     private lateinit var addTweetUseCase: AddTweetUseCase
+
     @Mock
     private lateinit var application: Application
     private lateinit var fakeTweets: List<Tweet>
@@ -53,8 +65,10 @@ class TweetsVIewModelTest {
         addCommentUseCase = Mockito.mock(AddCommentUseCase::class.java)
         addTweetUseCase = Mockito.mock(AddTweetUseCase::class.java)
         application = Mockito.mock(Application::class.java)
-        fakeTweets = listOf()
+//        val resources = RuntimeEnvironment.application.resources
+
         Dispatchers.setMain(mainThreadSurrogate)
+
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -68,6 +82,8 @@ class TweetsVIewModelTest {
     @Test
     fun should_return_null_list_if_both_remote_and_local_data_are_null() = runTest {
         //given
+        fakeTweets = listOf()
+
         `when`(fetchTweetsUseCase.invoke()).thenReturn(fakeTweets)
         val tweetsViewModel = TweetsViewModel(
             fetchTweetsUseCase = fetchTweetsUseCase,
@@ -78,6 +94,29 @@ class TweetsVIewModelTest {
         //when
         tweetsViewModel.fetchData()
         //then
-        assertSame(tweetsViewModel.tweets.value,fakeTweets)
+        assertSame(tweetsViewModel.tweets.value, fakeTweets)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun should_return_tweet_list_if_fetch_data() = runTest {
+        //given
+        val fakeTweets = Gson().fromJson<List<Tweet>>(
+            JSONResourceUtils().jsonResourceReader(
+                ApplicationProvider.getApplicationContext<Application>().resources, R.raw.tweets
+            ),
+            object : TypeToken<ArrayList<Tweet>>() {}.type
+        )
+        `when`(fetchTweetsUseCase.invoke()).thenReturn(fakeTweets)
+        val tweetsViewModel = TweetsViewModel(
+            fetchTweetsUseCase = fetchTweetsUseCase,
+            addCommentUseCase = addCommentUseCase,
+            addTweetUseCase = addTweetUseCase,
+            application = application
+        )
+        //when
+        tweetsViewModel.fetchData()
+        //then
+        assertSame(fakeTweets, tweetsViewModel.tweets.value)
     }
 }
