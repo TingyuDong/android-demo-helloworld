a7110f5f270e92a633ce6f27344487ef670c3c6b
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-12-08 18:30:59 +0800

    refactor:extract TweetsState

--- app/src/main/java/com/thoughtworks/androidtrain/tweets/TweetsViewModel.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/tweets/TweetsViewModel.kt
@@ -1,13 +1,9 @@
 package com.thoughtworks.androidtrain.tweets
 
-import android.app.Application
-import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.thoughtworks.androidtrain.data.model.Comment
-import com.thoughtworks.androidtrain.data.model.Sender
 import com.thoughtworks.androidtrain.data.model.Tweet
 import com.thoughtworks.androidtrain.usecase.AddCommentUseCase
 import com.thoughtworks.androidtrain.usecase.AddTweetUseCase
@@ -19,7 +15,6 @@ class TweetsViewModel(
     private val fetchTweetsUseCase: FetchTweetsUseCase,
     private val addCommentUseCase: AddCommentUseCase,
     private val addTweetUseCase: AddTweetUseCase,
-    private val application: Application
 ) : ViewModel() {
 
     private val _tweets = MutableLiveData(emptyList<Tweet>())
@@ -37,9 +32,9 @@ class TweetsViewModel(
         }
     }
 
-    fun saveComment(comment: Comment, tweetId: Int) {
+    fun saveComment(tweetId: Int, commentContent: String) {
         viewModelScope.launch(Dispatchers.Main) {
-            addCommentUseCase.invoke(comment, tweetId)
+            addCommentUseCase.invoke(tweetId, commentContent)
             _tweets.postValue(fetchTweetsUseCase.invoke())
         }
     }
@@ -49,17 +44,5 @@ class TweetsViewModel(
             addTweetUseCase.invoke(tweet)
             _tweets.postValue(fetchTweetsUseCase.invoke())
         }
-    }
-
-    fun getUserInfo(): Sender {
-        val settings: SharedPreferences = application.getSharedPreferences("UserInfo", 0)
-        val userName = settings.getString("UserName", "you")
-        val nick = settings.getString("Nick", "you")
-        val avatar = settings.getString("Avatar", "Avatar.png")
-        return Sender(
-            username = userName!!,
-            nick = nick!!,
-            avatar = avatar!!
-        )
     }
 }
