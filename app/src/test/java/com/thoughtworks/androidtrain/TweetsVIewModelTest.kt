00477bb6b2469a8a4db19631acc7863f0a210790
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-12-13 17:22:53 +0800

    fix:test fetchTweets function when it will create a new coroutine

--- app/src/test/java/com/thoughtworks/androidtrain/TweetsVIewModelTest.kt
+++ app/src/test/java/com/thoughtworks/androidtrain/TweetsVIewModelTest.kt
@@ -7,20 +7,14 @@ import androidx.test.core.app.ApplicationProvider
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.thoughtworks.androidtrain.data.model.Tweet
+import com.thoughtworks.androidtrain.rule.DispatchersRule
+import com.thoughtworks.androidtrain.tweets.TweetsViewModel
 import com.thoughtworks.androidtrain.usecase.AddCommentUseCase
 import com.thoughtworks.androidtrain.usecase.AddTweetUseCase
 import com.thoughtworks.androidtrain.usecase.FetchTweetsUseCase
 import com.thoughtworks.androidtrain.utils.JSONResourceUtils
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
-
 import org.junit.Assert.assertSame
 import org.junit.Before
 import org.junit.Rule
@@ -37,11 +31,11 @@ import kotlin.collections.ArrayList
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-//@RunWith(RobolectricTestRunner::class)
-//@RunWith(MockitoJUnitRunner::class)
 @Config(sdk = [29])
 @RunWith(AndroidJUnit4::class)
 class TweetsVIewModelTest {
+    private lateinit var tweetsViewModel: TweetsViewModel
+
     @Mock
     private lateinit var fetchTweetsUseCase: FetchTweetsUseCase
 
@@ -51,36 +45,25 @@ class TweetsVIewModelTest {
     @Mock
     private lateinit var addTweetUseCase: AddTweetUseCase
 
-    @Mock
-    private lateinit var application: Application
     private lateinit var fakeTweets: List<Tweet>
 
-    @OptIn(DelicateCoroutinesApi::class)
-    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
-
-//    @Rule
-//    val testRule: TestRule = InstantTaskExecutorRule()
     @get:Rule
     val executorRule = InstantTaskExecutorRule()
 
-    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    val dispatchersRule = DispatchersRule()
+
     @Before
     fun beforeEach() {
         fetchTweetsUseCase = Mockito.mock(FetchTweetsUseCase::class.java)
         addCommentUseCase = Mockito.mock(AddCommentUseCase::class.java)
         addTweetUseCase = Mockito.mock(AddTweetUseCase::class.java)
-        application = Mockito.mock(Application::class.java)
+        tweetsViewModel = TweetsViewModel(
+            fetchTweetsUseCase = fetchTweetsUseCase,
+            addCommentUseCase = addCommentUseCase,
+            addTweetUseCase = addTweetUseCase
+        )
 //        val resources = RuntimeEnvironment.application.resources
-
-        Dispatchers.setMain(mainThreadSurrogate)
-
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
-        mainThreadSurrogate.close()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -90,16 +73,10 @@ class TweetsVIewModelTest {
         fakeTweets = listOf()
 
         `when`(fetchTweetsUseCase.invoke()).thenReturn(fakeTweets)
-        val tweetsViewModel = TweetsViewModel(
-            fetchTweetsUseCase = fetchTweetsUseCase,
-            addCommentUseCase = addCommentUseCase,
-            addTweetUseCase = addTweetUseCase,
-            application = application
-        )
         //when
         tweetsViewModel.fetchData()
         //then
-        assertSame(tweetsViewModel.tweets.value, fakeTweets)
+        assertSame(fakeTweets, tweetsViewModel.tweets.value)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -113,18 +90,9 @@ class TweetsVIewModelTest {
             object : TypeToken<ArrayList<Tweet>>() {}.type
         )
         `when`(fetchTweetsUseCase.invoke()).thenReturn(fakeTweets)
-        val tweetsViewModel = TweetsViewModel(
-            fetchTweetsUseCase = fetchTweetsUseCase,
-            addCommentUseCase = addCommentUseCase,
-            addTweetUseCase = addTweetUseCase,
-            application = application
-        )
-        // This works but can be improved.
         //when
-        runBlocking {
-            tweetsViewModel.fetchTweet()
-        }
-        assertSame(fakeTweets, tweetsViewModel.tweets)
+        tweetsViewModel.fetchTweets()
         //then
+        assertSame(fakeTweets, tweetsViewModel.tweets.value)
     }
 }
