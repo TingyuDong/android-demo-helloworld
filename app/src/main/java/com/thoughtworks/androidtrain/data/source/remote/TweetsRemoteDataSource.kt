88fdf68db4f4f26588661396b06bccd98f8115e7
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-12-16 13:51:49 +0800

    refactor:delete unnecessary function

--- app/src/main/java/com/thoughtworks/androidtrain/data/source/remote/TweetsRemoteDataSource.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/data/source/remote/TweetsRemoteDataSource.kt
@@ -1,7 +1,6 @@
 package com.thoughtworks.androidtrain.data.source.remote
 
 import com.thoughtworks.androidtrain.data.model.Tweet
-import com.thoughtworks.androidtrain.data.source.TweetsDataSource
 import com.thoughtworks.androidtrain.service.TweetService
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -12,31 +11,14 @@ import okio.IOException
 
 class TweetsRemoteDataSource (
     private val ioDispatcher: CoroutineDispatcher,
-) : TweetsDataSource {
+) {
     private val observableTweets = MutableStateFlow(runBlocking { getTweets() })
 
-    suspend fun fetchRemoteTweets(): Result<List<Tweet>> {
-        val service = RetrofitClientInstance.getRetrofitInstance().create(TweetService::class.java)
-        return withContext(ioDispatcher) {
-            try {
-                val response = service.listTweets().execute()
-                if (response.isSuccessful) {
-                    return@withContext Result.success(response.body() ?: emptyList())
-                } else {
-                    return@withContext Result.failure(exception = Exception(response.message()))
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-                return@withContext Result.failure(exception = e)
-            }
-        }
-    }
-
-    override fun getTweetsStream(): Flow<Result<List<Tweet>>> {
+    fun getTweetsStream(): Flow<Result<List<Tweet>>> {
         return observableTweets
     }
 
-    override suspend fun getTweets(): Result<List<Tweet>> {
+    private suspend fun getTweets(): Result<List<Tweet>> {
         val service = RetrofitClientInstance.getRetrofitInstance().create(TweetService::class.java)
         return withContext(ioDispatcher) {
             try {
@@ -53,7 +35,7 @@ class TweetsRemoteDataSource (
         }
     }
 
-    override suspend fun refreshTasks() {
+    suspend fun refreshTasks() {
         observableTweets.value = getTweets()
     }
 }
