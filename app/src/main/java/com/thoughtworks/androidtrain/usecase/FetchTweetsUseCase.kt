83d23876037f68566410206899229579a93ce94b
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-12-16 14:25:44 +0800

    feat:could fetch localTweets

--- app/src/main/java/com/thoughtworks/androidtrain/usecase/FetchTweetsUseCase.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/usecase/FetchTweetsUseCase.kt
@@ -5,43 +5,47 @@ import com.thoughtworks.androidtrain.data.repository.CommentRepository
 import com.thoughtworks.androidtrain.data.repository.ImageRepository
 import com.thoughtworks.androidtrain.data.repository.SenderRepository
 import com.thoughtworks.androidtrain.data.repository.TweetRepository
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.map
 
 open class FetchTweetsUseCase(
     private val senderRepository: SenderRepository,
     private val commentRepository: CommentRepository,
     private val imageRepository: ImageRepository,
     private val tweetRepository: TweetRepository,
-    private val ioDispatcher: CoroutineDispatcher
 ) {
     open fun fetchRemoteTweets(): Flow<Result<List<Tweet>>> {
         return tweetRepository.getRemoteTweetsStream()
     }
 
-    suspend fun refreshTweets(){
-        tweetRepository.refreshTweets()
+    open fun fetchLocalTweets(): Flow<Result<List<Tweet>>> {
+        return tweetRepository.getLocalTweetsStream()
+            .map { tweetPOResult ->
+                tweetPOResult.map { tweetPOList ->
+                    tweetPOList.map { tweetPO ->
+                        val sender =
+                            tweetPO.senderName?.let { senderName ->
+                                senderRepository.getSender(
+                                    senderName
+                                )
+                            }
+                        val images = imageRepository.getImages(tweetPO.id)
+                        val comments = commentRepository.getComments(tweetPO.id)
+                        Tweet(
+                            id = tweetPO.id,
+                            content = tweetPO.content,
+                            sender = sender,
+                            images = images,
+                            comments = comments,
+                            error = tweetPO.error,
+                            unknownError = tweetPO.unknownError
+                        )
+                    }
+                }
+            }
     }
 
-    suspend fun fetchLocalTweets(): List<Tweet> = withContext(ioDispatcher) {
-        val allLocalTweets: List<Tweet> =
-            tweetRepository.getAllLocalTweets()
-                .filter { it.error == null && it.unknownError == null }.map {
-                    val sender =
-                        it.senderName?.let { senderName -> senderRepository.getSender(senderName) }
-                    val images = imageRepository.getImages(it.id)
-                    val comments = commentRepository.getComments(it.id)
-                    Tweet(
-                        id = it.id,
-                        content = it.content,
-                        sender = sender,
-                        images = images,
-                        comments = comments,
-                        error = it.error,
-                        unknownError = it.unknownError
-                    )
-                }
-        return@withContext allLocalTweets
+    suspend fun refreshTweets() {
+        tweetRepository.refreshTweets()
     }
 }
