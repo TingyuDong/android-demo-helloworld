23a3d2037c331bd9eee1cd18e7981057399e98fa
Tingyu Dong <tingyu.dong@thoughtworks.com>
2023-01-06 15:52:54 +0800

    fix: remove getUserInfo

--- app/src/main/java/com/thoughtworks/androidtrain/usecase/AddCommentUseCase.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/usecase/AddCommentUseCase.kt
@@ -1,9 +1,7 @@
 package com.thoughtworks.androidtrain.usecase
 
-import android.app.Application
-import android.content.SharedPreferences
-import com.thoughtworks.androidtrain.data.model.Sender
-import com.thoughtworks.androidtrain.data.repository.*
+import com.thoughtworks.androidtrain.TweetApplication
+import com.thoughtworks.androidtrain.data.repository.CommentsRepository
 import com.thoughtworks.androidtrain.data.source.local.room.entity.CommentPO
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -11,27 +9,15 @@ import kotlinx.coroutines.withContext
 open class AddCommentUseCase(
     private val commentRepository: CommentsRepository,
     private val ioDispatcher: CoroutineDispatcher,
-    private val application: Application
+    private val application: TweetApplication
 ) {
     suspend operator fun invoke(tweetId: Int, commentContent: String) = withContext(ioDispatcher) {
         val commentPO = CommentPO(
                 id = 0,
                 tweetId = tweetId,
                 content = commentContent,
-                senderName = getUserInfo().username
+                senderName = application.getUserInfo().username
             )
         commentRepository.addComment(commentPO)
-    }
-
-    private fun getUserInfo(): Sender {
-        val settings: SharedPreferences = application.getSharedPreferences("UserInfo", 0)
-        val userName = settings.getString("UserName", "Aiolia")
-        val nick = settings.getString("Nick", "Aio")
-        val avatar = settings.getString("Avatar", "Avatar.png")
-        return Sender(
-            username = userName!!,
-            nick = nick!!,
-            avatar = avatar!!
-        )
     }
 }
