17f08394879d17efc58e298ca881e79df189617a
Tingyu Dong <tingyu.dong@thoughtworks.com>
2023-01-10 09:19:42 +0800

    fix: fix the AppModules.kt

--- app/src/main/java/com/thoughtworks/androidtrain/modules/AppModules.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/modules/AppModules.kt
@@ -4,10 +4,10 @@ import androidx.room.Room
 import com.google.gson.GsonBuilder
 import com.thoughtworks.androidtrain.TweetApplication
 import com.thoughtworks.androidtrain.tweets.TweetsViewModel
-import com.thoughtworks.androidtrain.data.repository.CommentsRepository
-import com.thoughtworks.androidtrain.data.repository.ImagesRepository
-import com.thoughtworks.androidtrain.data.repository.SendersRepository
-import com.thoughtworks.androidtrain.data.repository.TweetsRepository
+import com.thoughtworks.androidtrain.data.repository.CommentsRepositoryImpl
+import com.thoughtworks.androidtrain.data.repository.ImagesRepositoryImpl
+import com.thoughtworks.androidtrain.data.repository.SendersRepositoryImpl
+import com.thoughtworks.androidtrain.data.repository.TweetsRepositoryImpl
 import com.thoughtworks.androidtrain.data.source.local.room.AppDatabase
 import com.thoughtworks.androidtrain.data.source.local.room.CommentsLocalDataSource
 import com.thoughtworks.androidtrain.data.source.local.room.ImagesLocalDataSource
@@ -19,7 +19,6 @@ import com.thoughtworks.androidtrain.service.TweetsApiHelperImpl
 import com.thoughtworks.androidtrain.usecase.AddCommentUseCase
 import com.thoughtworks.androidtrain.usecase.AddTweetUseCase
 import com.thoughtworks.androidtrain.usecase.FetchTweetsUseCase
-import com.thoughtworks.androidtrain.utils.OkHttpUtils
 import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -29,27 +28,50 @@ import retrofit2.converter.gson.GsonConverterFactory
 private const val DATABASE_NAME = "tweet"
 private const val BASE_URL = "https://thoughtworks-mobile-2018.herokuapp.com"
 
-val appModules = module {
+val retrofitModules = module {
     single {
         GsonBuilder()
             .serializeNulls()
             .create()
     }
+
     single {
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create(get()))
             .baseUrl(BASE_URL)
             .build()
     }
+}
+
+val apiHelperModules = module {
     single {
         get<Retrofit>().create(TweetService::class.java)
     }
+
     single {
         TweetsApiHelperImpl(get())
     }
+}
+
+val daoModules = module {
     single {
-        TweetsRemoteDataSource(get(), get())
+        get<AppDatabase>().tweetDao()
     }
+
+    single {
+        get<AppDatabase>().commentDao()
+    }
+
+    single {
+        get<AppDatabase>().imageDao()
+    }
+
+    single {
+        get<AppDatabase>().senderDao()
+    }
+}
+
+val localDataModules = module {
     single {
         Room.databaseBuilder(
             get(),
@@ -57,61 +79,89 @@ val appModules = module {
             DATABASE_NAME
         ).build()
     }
-    single {
-        OkHttpUtils(get())
-    }
-    single {
-        get<AppDatabase>().tweetDao()
-    }
-    single {
-        get<AppDatabase>().commentDao()
-    }
-    single {
-        get<AppDatabase>().imageDao()
-    }
-    single {
-        get<AppDatabase>().senderDao()
-    }
+
+    includes(daoModules)
+
     single {
         CommentsLocalDataSource(get())
     }
+
     single {
         ImagesLocalDataSource(get())
     }
+
     single {
         SendersLocalDataSource(get())
     }
+
     single {
         TweetsLocalDataSource(get())
     }
+}
+
+val remoteDataSourceModules = module {
+    includes(
+        retrofitModules,
+        apiHelperModules,
+    )
+
     single {
-        CommentsRepository(get(), get())
+        TweetsRemoteDataSource(get(), get())
     }
+}
+
+val dataSourceModules = module {
+    includes(localDataModules, remoteDataSourceModules)
+}
+
+val repositoryModules = module {
     single {
-        ImagesRepository(get())
+        CommentsRepositoryImpl(get(), get())
     }
+
     single {
-        SendersRepository(get())
+        ImagesRepositoryImpl(get())
     }
+
     single {
-        TweetsRepository(get(), get())
+        SendersRepositoryImpl(get())
     }
+
+    single {
+        TweetsRepositoryImpl(get(), get(), get(), get(), get())
+    }
+}
+
+val useCaseModules = module {
+    factory {
+        AddCommentUseCase(get(), get(), get())
+    }
+
+    factory {
+        AddTweetUseCase(get(), get(), get(), get(), get())
+    }
+
+    factory {
+        FetchTweetsUseCase(get(), get())
+    }
+}
+
+val appModules = module {
+    includes(
+        dataSourceModules,
+        repositoryModules,
+        useCaseModules
+    )
+
     single {
         Dispatchers.IO
     }
-    single {
-        AddCommentUseCase(get(), get(), get())
-    }
-    single {
-        AddTweetUseCase(get(), get(), get(), get(), get())
-    }
-    single {
-        FetchTweetsUseCase(get(), get(), get(), get())
-    }
+
     single {
         TweetApplication()
     }
+
     viewModel {
-        TweetsViewModel(get(), get(), get())
+        TweetsViewModel(get(), get(), get(), get())
     }
 }
