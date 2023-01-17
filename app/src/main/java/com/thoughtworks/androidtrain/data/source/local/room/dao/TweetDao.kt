cafe5c4699a787ef438c2f7130bfbc8b9c7fe377
Tingyu Dong <tingyu.dong@thoughtworks.com>
2023-01-17 11:17:07 +0800

    refactor: ensure that the query is atomic

--- app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/dao/TweetDao.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/dao/TweetDao.kt
@@ -4,12 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.thoughtworks.androidtrain.data.source.local.room.TweetWithSenderAndCommentsAndImages
 import com.thoughtworks.androidtrain.data.source.local.room.entity.TweetPO
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TweetDao {
+    @Transaction
     @Query("SELECT * FROM tweets")
     fun observeTweets(): Flow<List<TweetWithSenderAndCommentsAndImages>>
 
