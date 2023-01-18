64903f4110e04c519c3686ee3b2fb4f1bc0bb866
Tingyu Dong <tingyu.dong@thoughtworks.com>
2023-01-18 15:19:47 +0800

    fix: change data table name

--- app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/entity/CommentPO.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/entity/CommentPO.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "comments")
+@Entity(tableName = "comment")
 data class CommentPO(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
--- app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/entity/TweetPO.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/entity/TweetPO.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tweets")
+@Entity(tableName = "tweet")
 data class TweetPO(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
